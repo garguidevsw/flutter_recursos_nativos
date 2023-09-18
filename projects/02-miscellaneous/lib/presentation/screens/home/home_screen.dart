3f5c95b97bd20fca9439651b992da232d70f4704
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2023-09-17 18:11:18 -0600

    feat(02-miscellaneous): add routes and screens

---
+++ projects/02-miscellaneous/lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Miscellaneous'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.push('/permissions');
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
